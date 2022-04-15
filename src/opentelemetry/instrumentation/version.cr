42597b3995be09b3ed32fc78b73fe2b4db5bb070
Kirk Haines <wyhaines@gmail.com>
2022-04-15 15:49:05 -0600

    This one weird trick...no, really. I don't understand why I had to do this.z

--- src/opentelemetry/instrumentation/version.cr
+++ src/opentelemetry/instrumentation/version.cr
@@ -1,7 +1,6 @@
 module OpenTelemetry
   module Instrumentation
     {% begin %}
-    # Pull the version directly from Git.
     VERSION = {{ read_file("#{__DIR__}/../../../VERSION").chomp }}
     {% end %}
   end
