30170fc1b64f7c6013912e2eaf0747b822702ede
Kirk Haines <wyhaines@gmail.com>
2022-05-09 20:25:00 -0600

    Debugging.

--- src/opentelemetry/instrumentation/db/stefanwille_redis.cr
+++ src/opentelemetry/instrumentation/db/stefanwille_redis.cr
@@ -27,7 +27,6 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
     end
 
     pp "Pass 2"
-    pp Redis::VERSION
 
     if_defined?(Redis::VERSION) do
       pp "GO STEFAN"
@@ -35,8 +34,8 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
       # Monkeypatch a few things so that useful information is available for the spans.
       # TODO: See if the maintainers would like some version of these patches contributed back to the main project.
       class Redis
-        getter database : Int32?
-        getter uri : URI?
+        getter database : Int32? = nil
+        getter uri : URI? = nil
 
         def initialize(@host = "localhost", @port = 6379, @unixsocket : String? = nil, @password : String? = nil,
                        @database : Int32? = nil, url = nil, @ssl = false, @ssl_context : OpenSSL::SSL::Context::Client? = nil,
@@ -61,8 +60,13 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
         end
       end
 
+      class Redis::Connection
+        property database : Int32? = nil
+        property uri : URI? = nil
+      end
+
       class Redis::Future
-        getter trace_parent : OpenTelemetry::Propagation::TraceContext::TraceParent? = nil
+        property trace_parent : OpenTelemetry::Propagation::TraceContext::TraceParent? = nil
       end
 
       class Redis::Strategy::SingleStatement
@@ -89,8 +93,8 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
                                     end
 
             span["db.system"] = "redis"
-            span["db.statement"] = command.map(&.inspect_unquoted).join(' ')
-            span["db.redis.database_index"] = (@uri.path.presence || "/")[1..].presence
+            span["db.statement"] = request.map(&.to_s.inspect_unquoted).join(' ')
+            span["db.redis.database_index"] = (@connection.@uri.try(&.path.presence) || "/")[1..].presence.to_s
 
             previous_def
           end
@@ -99,7 +103,7 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
 
       class Redis::Strategy::Pipeline
         trace("command") do
-          OpenTelemetry.trace.in_span("Redis #{request[0..1]?.join(' ')}") do |span|
+          OpenTelemetry.trace.in_span("Redis #{request[0..1]? ? request[0..1].join(' ') : ""}") do |span|
             span.producer!
             socket = @connection.@socket
             span["net.peer.name"] = case socket
@@ -117,16 +121,18 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
                                     when TCPSocket, OpenSSL::SSL::Socket::Client
                                       "ip_tcp"
                                     else
-                                      @socket.class.name # Generic fallback, but is unlikely to happen
+                                      socket.class.name # Generic fallback, but is unlikely to happen
                                     end
 
             span["db.system"] = "redis"
-            span["db.statement"] = command.map(&.inspect_unquoted).join(' ')
-            span["db.redis.database_index"] = (@uri.path.presence || "/")[1..].presence
+            span["db.statement"] = request.map(&.to_s.inspect_unquoted).join(' ')
+            span["db.redis.database_index"] = (@connection.@uri.try(&.path.presence) || "/")[1..].presence.to_s
 
             future = previous_def
 
-            future.trace_parent = OpenTelemetry::Propagation::TraceContext::TracePrarent.new(span.context)
+            future.trace_parent = OpenTelemetry::Propagation::TraceContext::TraceParent.from_span_context(span.context)
+
+            future
           end
         end
       end
@@ -138,11 +144,15 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_STEFANWILLE_REDIS") do
             parent = OpenTelemetry::Span.build do |pspan|
               pspan.is_recording = false
 
-              pspan.context.trace_id = trace_parent.trace_id
-              pspan.context.span_id = trace_parent.span_id
+              if tptid = trace_parent.try &.trace_id
+                pspan.context.trace_id = tptid
+                span.context.trace_id = tptid
+              end
+              if tpsid = trace_parent.try &.span_id
+                pspan.context.span_id = tpsid
+              end
             end
             span.parent = parent
-            span.context.trace_id = trace_parent.trace_id
 
             previous_def
           end
