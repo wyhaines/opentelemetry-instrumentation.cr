987fd3861101ab540d2e0574af004f8691be079a
Kirk Haines <wyhaines@gmail.com>
2022-05-11 08:34:21 -0600

    Add a fiber based GC instrument that, by default, records every 5 minutes.

--- src/opentelemetry/instrumentation/crystal/gc.cr
+++ src/opentelemetry/instrumentation/crystal/gc.cr
@@ -30,7 +30,31 @@ unless_enabled?("OTEL_CRYSTAL_DISABLE_INSTRUMENTATION_GC") do
   end
 
   if_version?(Crystal, :>=, "1.0.0") do
-    module GC
+    # It doesn't appear easy/possible to hook into the GC in a way that lets us
+    # record any stats in the moment before GC runs. If a means to do this can
+    # be achieved, the code below will become more deterministic. For now, though,
+    # the GC instrument will just be a fiber that wakes periodically to record a
+    # span with the GC stats. If there is an existing trace, the span will be
+    # inserted into it. If there is no currently active trace, the GC stats will
+    # create a single-span trace.
+    spawn(name: "GC Span Recording") do
+      loop do
+        stats = GC.prof_stats
+        OpenTelemetry.trace.in_span("GC Stats") do |span|
+          span["gc.bytes_before_gc"] = stats.bytes_before_gc
+          span["gc.bytes_reclaimed_sinc_gc"] = stats.bytes_reclaimed_since_gc
+          span["gc.bytes_since_gc"] = stats.bytes_since_gc
+          span["gc.free_bytes"] = stats.free_bytes
+          span["gc.gc_no"] = stats.gc_no
+          span["gc.heap_size"] = stats.heap_size
+          span["gc.markers_m1"] = stats.markers_m1
+          span["gc.non_gc_bytes"] = stats.non_gc_bytes
+          span["gc.reclaimed_bytes_before_gc"] = stats.reclaimed_bytes_before_gc
+          span["gc.unmapped_bytes"] = stats.unmapped_bytes
+        end
+    
+        sleep ENV["OTEL_CRYSTAL_GC_SPAN_RECORDING_INTERVAL"]?.try(&.to_i?) ? ENV["OTEL_CRYSTAL_GC_SPAN_RECORDING_INTERVAL"].to_i : 300 
+      end
     end
   end
 end
