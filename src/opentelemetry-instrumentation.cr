5e3ad708e2c39bac0a7414dc7dd583616bfb28f7
Kirk Haines <wyhaines@gmail.com>
2022-05-11 06:42:16 -0600

    Remove a diagnostic print.

--- src/opentelemetry-instrumentation.cr
+++ src/opentelemetry-instrumentation.cr
@@ -4,6 +4,5 @@ require "./ext/*"
 require "./opentelemetry-instrumentation/log_backend"
 
 macro finished
-  puts "DOING INSTRUMENTATION REQUIRES"
   require "./opentelemetry/instrumentation/**"
 end
