89f39713105c064663be508df509c50357b68bef
Kirk Haines <wyhaines@gmail.com>
2022-06-02 09:32:24 -0600

    Remove the finished block, as it causes the instrumentation to simply not be loaded in many cases.

--- src/opentelemetry-instrumentation.cr
+++ src/opentelemetry-instrumentation.cr
@@ -4,6 +4,11 @@ require "tracer"
 require "./ext/*"
 require "./opentelemetry-instrumentation/log_backend"
 
-macro finished
-  require "./opentelemetry/instrumentation/**"
-end
+# There is a puzzle here. Ideally, this require is executed from a `finished` block.
+# However, it is possible, and even likely that such a block, right here, will never be executed.
+# The reason is that if code execution starts in a place that is ahead of where that block ends
+# up being injected into the final code, then execution may _never_ reach the code that is
+# required in said block.
+# The onus will be on the user of the instrumentation to ensure that the instrumentation is
+# required last, after all other code.
+require "./opentelemetry/instrumentation/**"
